bbb032d440854692d2d84467f350fe0cf1aaf84b
Khaled Waleed <unknownerror340@gmail.com>
2024-03-25 00:05:22 +0300

    chore: store code cleanup

--- src/stores/tasks.ts
+++ src/stores/tasks.ts
@@ -1,43 +1,51 @@
 import dayjs from 'dayjs'
-import type { Task } from '~/types/main'
+import { defineStore } from 'pinia'
 import { exampleTask } from '~/data'
+import type { Status, Task } from '~/types/main'
 
 export const useTaskStore = defineStore('task', () => {
-  const tasks = ref<Task[]>([
-    exampleTask,
-  ])
+  const tasks = ref<Task[]>([exampleTask])
 
-  const todayTasks = computed(() => tasks.value.filter(task => dayjs(task.creationDate).isSame(dayjs(), 'day') && task.status !== 'trashed' && task.status !== 'wontdo'))
-  const thisWeekTasks = computed(() => tasks.value.filter(task => dayjs(task.creationDate).isSame(dayjs(), 'week') && task.status !== 'trashed' && task.status !== 'wontdo'))
-  const completedTasks = computed(() => tasks.value.filter(task => task.status === 'done'))
-  const trashedTasks = computed(() => tasks.value.filter(task => task.status === 'trashed'))
-  const wontDoTasks = computed(() => tasks.value.filter(task => task.status === 'wontdo'))
-  const sortedByDateTasks = computed(() => tasks.value.sort((a, b) => new Date(b.creationDate).getTime() - new Date(a.creationDate).getTime()))
-  const sortedByPriorityTasks = computed(() => tasks.value.sort((a, b) => b.priority - a.priority))
+  const todayTasks = computed(() => filterTasks('day'))
+  const thisWeekTasks = computed(() => filterTasks('week'))
+  const completedTasks = computed(() => filterStatus('done'))
+  const trashedTasks = computed(() => filterStatus('trashed'))
+  const wontDoTasks = computed(() => filterStatus('wontdo'))
+  const sortedByDateTasks = computed(() => sortTasks('date'))
+  const sortedByPriorityTasks = computed(() => sortTasks('priority'))
+
+  function filterTasks(unit: string) {
+    return tasks.value.filter(task => dayjs(task.creationDate).isSame(dayjs(), unit) && !['trashed', 'wontdo'].includes(task.status))
+  }
+
+  function filterStatus(status: string) {
+    return tasks.value.filter(task => task.status === status)
+  }
+
+  function sortTasks(criteria: string) {
+    return [...tasks.value].sort((a, b) => {
+      if (criteria === 'date')
+        return new Date(b.creationDate).getTime() - new Date(a.creationDate).getTime()
+      else if (criteria === 'priority')
+        return b.priority - a.priority
+      return 0
+    })
+  }
 
   function addTask(task: Task) {
     tasks.value.unshift(task)
   }
 
   function editTaskTitle(id: number, title: string) {
-    const index = tasks.value.findIndex(task => task.id === id)
-
-    if (index !== -1)
-      tasks.value[index].title = title
+    const task = tasks.value.find(task => task.id === id)
+    if (task)
+      task.title = title
   }
 
-  function toggleTrashTask(id: number) {
+  function toggleTaskStatus(id: number, status: Status) {
     const task = tasks.value.find(task => task.id === id)
-
     if (task)
-      task.status = task.status === 'trashed' ? 'todo' : 'trashed'
-  }
-
-  function toggleWontDoTask(id: number) {
-    const task = tasks.value.find(task => task.id === id)
-
-    if (task)
-      task.status = task.status === 'wontdo' ? 'todo' : 'wontdo'
+      task.status = task.status === status ? 'todo' : status
   }
 
   function deleteTask(id: number) {
@@ -45,15 +53,19 @@ export const useTaskStore = defineStore('task', () => {
   }
 
   function toggleCompleteTask(id: number) {
-    const task = tasks.value.find(task => task.id === id)
+    toggleTaskStatus(id, 'done')
+  }
 
-    if (task)
-      task.status = task.status === 'done' ? 'todo' : 'done'
+  function toggleTrashTask(id: number) {
+    toggleTaskStatus(id, 'trashed')
+  }
+
+  function toggleWontDoTask(id: number) {
+    toggleTaskStatus(id, 'wontdo')
   }
 
   onMounted(() => {
     const storedTasks = localStorage.getItem('tasks') ?? JSON.stringify(tasks.value)
-
     tasks.value = JSON.parse(storedTasks)
   })
 
@@ -68,17 +80,14 @@ export const useTaskStore = defineStore('task', () => {
     completedTasks,
     trashedTasks,
     wontDoTasks,
-
     sortedByDateTasks,
     sortedByPriorityTasks,
-
     addTask,
     deleteTask,
     editTaskTitle,
     toggleCompleteTask,
     toggleTrashTask,
     toggleWontDoTask,
-
   }
 })
 
