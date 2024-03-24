ea4a4bd40b1f938488efef88e691053ca4dd6796
Khaled Waleed <unknownerror340@gmail.com>
2024-03-24 23:58:07 +0300

    fix: copyright misplaced on mobile

--- src/components/CopyRight.vue
+++ src/components/CopyRight.vue
@@ -5,7 +5,7 @@ const copyRight = 'CC BY-NC-SA 4.0 2019-PRESENT © Khaled Waleed'
 <template>
   <span
     text="slate400 dark:slate600 xs"
-    class="duration-300 z3 fixed bottom-4 right-4 "
+    class="fixed bottom-4 right-1/2 z3 min-w-max translate-x-1/2 md:translate-x-0 duration-300 md:right-4"
     p="x4"
     v-text="copyRight"
   />
